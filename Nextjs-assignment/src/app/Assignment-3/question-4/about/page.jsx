293ec33758f9c2696ba3f3ddb9b955a786afba15
Manoj Kumar <manoj.kumar@manojvsr-32.successivetech.com>
2025-07-16 16:17:14 +0530

    assignment-3 chaanges updated

--- Nextjs-assignment/src/app/Assignment-3/question-4/about/page.jsx
+++ Nextjs-assignment/src/app/Assignment-3/question-4/about/page.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>About Page</div>
+    <>
+        <div>About Page</div>
+        <p>Welcome to our Product List application! This platform is designed to help you explore and manage a wide range of products efficiently. With a simple and user-friendly interface, you can browse, view details, and stay informed about various items. Whether you're learning web development or building your own inventory system, this app serves as a practical example. Feel free to log in and get started with your product browsing journey today!
+
+</p>
+    </>
+
   )
 }
 
-export default Page
+export default Page;

73af9d88f6f19328fa424e880550e89d345374fe
Manoj Kumar <manoj.kumar@manojvsr-32.successivetech.com>
2025-07-17 06:45:30 +0530

    assignment-3 changes updated

--- Nextjs-assignment/src/app/Assignment-3/question-4/about/page.jsx
+++ Nextjs-assignment/src/app/Assignment-3/question-4/about/page.jsx
@@ -4,9 +4,15 @@ const Page = () => {
   return (
     <>
         <div>About Page</div>
-        <p>Welcome to our Product List application! This platform is designed to help you explore and manage a wide range of products efficiently. With a simple and user-friendly interface, you can browse, view details, and stay informed about various items. Whether you're learning web development or building your own inventory system, this app serves as a practical example. Feel free to log in and get started with your product browsing journey today!
+        
+        <p>   This login page allows users to securely enter their credentials and gain access to protected routes. 
+        It uses a simple form with two input fields for username and password. When the correct login details 
+        are entered — specifically <b>manojkumar65@gmail.com</b> as the username and <b>123456</b> as the password — 
+        the user is redirected to this About page. The login logic is handled by a custom `useAuth` hook, 
+        ensuring authentication is verified before granting access to restricted content. Incorrect credentials 
+        trigger an error message to guide users.
+        </p>
 
-</p>
     </>
 
   )
